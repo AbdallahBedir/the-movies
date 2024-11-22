b2baf49ee8e7d1b8f4d8d43fb25525fe2e362da2
abdallahbedir2 <abdallah.bedir@shezlong.com>
2024-11-22 18:18:17 +0200

    add nest app to compose file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import AppDataSource from './typeorm.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const dataSource = await AppDataSource.initialize();
+
+  // run migrations, create tables,...
+  const migrations = await dataSource.runMigrations();
+  console.log(`migrations`, migrations);
+
   await app.listen(8080);
 }
 bootstrap();
